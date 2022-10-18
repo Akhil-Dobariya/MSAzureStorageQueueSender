5c26ee547b8076e417682378bb7789ad0a757a3f
Akhil-Dobariya <akhil.dobariya@outlook.com>
2022-10-18 19:41:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Queues;
+using System;
+
+namespace MSAzureStorageQueueSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "ConnectString Or SAS Token";
+            string queueName = "QueueName";
+
+            QueueClient client = new QueueClient(connectionString, queueName);
+
+            client.CreateIfNotExists();
+
+            if (client.Exists())
+            {
+                for (int i = 0; i < 500; i++)
+                {
+                    client.SendMessage($"Test Queue Message {i}");
+
+                    Console.WriteLine($"Test Queue Message {i}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
